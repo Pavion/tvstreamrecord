9c1f3ec704ceb22f83e02db1252395caf3e4dea1
Pavion <priakhine@yahoo.de>
2015-06-05 12:04:57 +0200

    1.1.0 Version commit

--- tvstreamrecord.py
+++ tvstreamrecord.py
@@ -54,7 +54,7 @@ localdatetime = "%d.%m.%Y %H:%M:%S"
 localtime = "%H:%M"
 localdate = "%d.%m.%Y"
 dayshown = datetime.combine(date.today(), time.min)
-version = '1.0.9'
+version = '1.1.0'
 
 @route('/live/<filename>')
 def server_static9(filename):

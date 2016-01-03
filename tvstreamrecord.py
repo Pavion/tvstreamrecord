39c559623c5c18f62b851ad3985afba190ee7387
Pavion <priakhine@yahoo.de>
2016-01-03 12:09:33 +0100

    Versioning

--- tvstreamrecord.py
+++ tvstreamrecord.py
@@ -57,7 +57,7 @@ localtime = "%H:%M"
 localdate = "%d.%m.%Y"
 dayshown = datetime.combine(date.today(), time.min)
 shutdown = False 
-version = '1.2.1'
+version = '1.2.2'
 
 @route('/live/<filename>')
 def server_static9(filename):
